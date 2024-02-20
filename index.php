014e38fb06a8b46c346251cfa864f2e78a3f0801
mariocstweb <mariocstmra@gmail.com>
2024-02-20 15:48:46 +0100

    added table with foreach

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <?php
+require 'data/data.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Hotel</title>
 </head>
 <body>
-  
+  <div class="container p-5">
+  <h1 class="text-center">Hotels</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Parking</th>
+          <th scope="col">Vote</th>
+          <th scope="col">Distance to center</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($hotels as $index => $hotel) : ?>
+        <tr>
+          <td><?= $hotel['name'] ?></td>
+          <td><?= $hotel['description'] ?></td>
+          <td><?= $hotel['parking'] ? '<i class="fa-regular fa-circle-check"></i>' : '<i class="fa-regular fa-circle-xmark"></i>' ?></td>
+          <td><?= $hotel['vote'] ?></td>
+          <td><?= $hotel['distance_to_center'] ?> km</td>
+        </tr>
+        <?php endforeach ?>
+      </tbody>
+    </table>
+  </div>
 </body>
 </html>
